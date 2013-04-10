8faf5137e80ed0a8d19cb27f577cd4055e2ed06b
Rik Schennink <rschennink@mirabeau.nl>
2013-04-10 14:53:33 +0200

    html updated

--- gruntfile.js
+++ gruntfile.js
@@ -76,6 +76,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default',['concat','copy','uglify']);
+    grunt.registerTask('default',['concat','copy','uglify','requirejs']);
 
 };
