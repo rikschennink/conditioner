71b97f422a2c717826f4f838678aa6f27d501b6b
Rik Schennink <rikschennink@gmail.com>
2013-05-06 13:32:41 +0200

    fixing map load method

--- demo/js/ui/Map.js
+++ demo/js/ui/Map.js
@@ -14,6 +14,9 @@ define(['conditioner'],function(conditioner){
         // backup content
         this._inner = this._element.innerHTML;
 
+        // remember if loading
+        this._loading = false;
+
         // load map
         this._load(this._element.getAttribute('href'));
     };
@@ -28,6 +31,12 @@ define(['conditioner'],function(conditioner){
             return;
         }
 
+        // now loading map
+        this._loading = true;
+
+        // set to loading
+        this._element.innerHTML = 'Loading map';
+
         // setup lat lng
         var qs = url.match(/[\d]*\.?[\d]+/g),
 
@@ -41,7 +50,22 @@ define(['conditioner'],function(conditioner){
         };
 
         // clear
-        this._element.innerHTML = '<img src="http://maps.googleapis.com/maps/api/staticmap?center=' + position.coords.latitude + ',' + position.coords.longitude + '&zoom=' + position.zoom + '&size=' + 500 + 'x' + 300 + '&maptype=roadmap&sensor=false" alt="" class="map"/>';
+        var self = this;
+        var map = document.createElement('img');
+        map.setAttribute('alt','');
+        map.className = 'map';
+        map.onload = function() {
+
+            // if not loading don't append
+            if (!self._loading) {
+                return;
+            }
+
+            // append map image
+            self._element.innerHTML = '';
+            self._element.appendChild(map);
+        };
+        map.src = 'http://maps.googleapis.com/maps/api/staticmap?center=' + position.coords.latitude + ',' + position.coords.longitude + '&zoom=' + position.zoom + '&size=' + 500 + 'x' + 300 + '&maptype=roadmap&sensor=false';
 
     };
 
@@ -50,6 +74,9 @@ define(['conditioner'],function(conditioner){
 
         // call ModuleBase unload method
         _parent.prototype.unload.call(this);
+
+        // not loading anymore
+        this._loading = false;
 
         // restore content
         this._element.innerHTML = this._inner;
