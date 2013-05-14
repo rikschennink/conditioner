122eb15b6469eea51663dcf9628eb06f104e6faf
Rik Schennink <rikschennink@gmail.com>
2013-05-14 08:46:20 +0200

    moving docs to pages branch

--- docs/js/shim/Array.isArray.js
+++
@@ -1,6 +0,0 @@
-// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/isArray
-if(!Array.isArray) {
-    Array.isArray = function (vArg) {
-        return Object.prototype.toString.call(vArg) === "[object Array]";
-    };
-}
