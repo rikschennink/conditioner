e586c891dac628713968a64570aa073eb129fda7
Rik Schennink <rikschennink@gmail.com>
2013-05-27 22:45:19 +0200

    updated introduction

--- gruntfile.js
+++ gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
 
                     baseUrl:'js/',
                     paths:{
-                        'conditioner':'lib/rikschennink/conditioner-0.8.6.min'
+                        'conditioner':'lib/rikschennink/conditioner-0.8.7.min'
                     },
 
                     name:'lib/jrburke/require',
