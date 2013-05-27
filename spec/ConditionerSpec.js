a17a2d1911a73f4cca069141748211fa0271e577
Rik Schennink <rikschennink@gmail.com>
2013-05-27 16:00:44 +0200

    added jasmine DOM parse test

--- spec/ConditionerSpec.js
+++ spec/ConditionerSpec.js
@@ -3,21 +3,41 @@
 
 describe('Conditioner',function(){
 
+    it('will return exactly two nodes when asked to parse a certain part of the DOM',function(){
+
+        // arrange
+        var a = document.createElement('div');
+        a.setAttribute('data-module','mock/jasmine');
+
+        var b = document.createElement('div');
+        b.setAttribute('data-module','mock/jasmine');
+
+        var group = document.createElement('div');
+        group.appendChild(a);
+        group.appendChild(b);
+
+        // act
+        var conditioner = new Conditioner();
+        var results = conditioner.loadModules(group);
+
+        // assert
+        expect(function(){return typeof results !== 'undefined'}).toBeTruthy();
+        expect(results.length).toEqual(2);
+
+    });
+
     it('will return the right order of nodes when using the data-priority attribute',function(){
 
         // arrange
         var a = document.createElement('div');
-        a.id = 'a';
         a.setAttribute('data-module','mock/jasmine');
         a.setAttribute('data-priority','-1');
 
         var b = document.createElement('div');
-        b.id = 'b';
         b.setAttribute('data-priority','1');
         b.setAttribute('data-module','mock/jasmine');
 
         var c = document.createElement('div');
-        c.id = 'c';
         c.setAttribute('data-module','mock/jasmine');
 
         var group = document.createElement('div');
